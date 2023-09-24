ae623dbc013fce3ed4dec1352ce926e6e8f026f6
VovaRuzhevich <vova.ruzhevich@gmail.com>
2023-09-24 19:15:25 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Test_Task_Paletch_Inc"
+include(":app")
+ 
